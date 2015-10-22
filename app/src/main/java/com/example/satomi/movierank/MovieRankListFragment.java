8fde3fb95fb248f57f99e02fa46017025447ea03
rei___yu <satomi.i.sakura@gmail.com>
2015-10-22 18:30:58 +0900

    fetch data from api

--- app/src/main/java/com/example/satomi/movierank/MovieRankListFragment.java
+++ app/src/main/java/com/example/satomi/movierank/MovieRankListFragment.java
@@ -2,13 +2,26 @@ package com.example.satomi.movierank;
 
 import android.app.ListFragment;
 import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -27,6 +40,18 @@ public class MovieRankListFragment extends ListFragment {
         mMovieRankAdapter = new MovieRankAdapter(getActivity(), MovieSet.list);
         setListAdapter(mMovieRankAdapter);
     }
+
+    private void updateRank() {
+        FetchRankTask getRankTask = new FetchRankTask();
+        getRankTask.execute();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateRank();
+    }
+
 
     public class MovieRankAdapter extends ArrayAdapter<String> {
 
@@ -49,6 +74,122 @@ public class MovieRankListFragment extends ListFragment {
             textView.setText(getItem(position));
 
             return view;
+        }
+    }
+
+    public class FetchRankTask extends AsyncTask<String, Void, String[]> {
+
+        private final String LOG_TAG = FetchRankTask.class.getSimpleName();
+
+        private String[] getMovieRankDataFromJson(String movieRankStr)
+                throws JSONException {
+
+            final String OWM_RESULTS = "results";
+            final String OWM_TITLE = "title";
+            final String OWM_OVERVIEW = "overview";
+            final String OWM_POSTER_PATH = "poster_path";
+            final String OWM_RELEASE_DATE = "release_date";
+            final String OWM_VOTE_AVERAGE = "vote_average";
+
+            JSONObject movieRankJson = new JSONObject(movieRankStr);
+            JSONArray movieRankArray = movieRankJson.getJSONArray(OWM_RESULTS);
+
+            String[] resultStrs = new String[20];
+
+            for(int i = 0; i < 20; i++) {
+                JSONObject movieInfo = movieRankArray.getJSONObject(i);
+
+                String title = movieInfo.getString(OWM_TITLE);
+                String release_date = movieInfo.getString(OWM_RELEASE_DATE);
+                String vote_ave = movieInfo.getString(OWM_VOTE_AVERAGE);
+                String synopsis  = movieInfo.getString(OWM_OVERVIEW);
+                String poster_path = movieInfo.getString(OWM_POSTER_PATH);
+
+                resultStrs[i] = "title: " + title + "\nrelease date: " + release_date + "\nvote average: " + vote_ave + "\nsynopsis: " + synopsis + "\nposter: " + poster_path;
+            }
+
+            return resultStrs;
+        }
+
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String movieRankStr = null;
+
+            String rank_popurality = "popularity.desc";
+
+            try {
+                final String TMD_BASE_URL_RANK = "https://api.themoviedb.org/3/discover/movie";
+                final String TMD_BASE_URL_POSTER = "http://image.tmdb.org/t/p/w500/";
+                final String SORT_PARAM = "sort_by";
+                final String API_KEY = "api_key";
+
+                Uri builtUri = Uri.parse(TMD_BASE_URL_RANK).buildUpon()
+                        .appendQueryParameter(SORT_PARAM, rank_popurality)
+                        .appendQueryParameter(API_KEY, BuildConfig.THE_MOVIE_DB_API_KEY)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                movieRankStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getMovieRankDataFromJson(movieRankStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                mMovieRankAdapter.clear();
+
+                for(String movieSummary : result) {
+                    mMovieRankAdapter.add(movieSummary);
+                }
+            }
         }
     }
 }
